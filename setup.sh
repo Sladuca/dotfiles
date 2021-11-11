0004e9e054307711886c6a88216d5cf0ed918425
Sebastien La Duca <sladuca777@gmail.com>
2021-11-11 16:31:56 +0000

    add stuff

---
+++ setup.sh
@@ -0,0 +1,3 @@
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y git-all build-essential zsh neovim mosh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
---
+++ vim/setup.sh
@@ -0,0 +1,6 @@
+mkdir -p ~/.config/nvim
+cp -r ./include ~/.config/nvim
+cp ./init.vim ~/.config/nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
