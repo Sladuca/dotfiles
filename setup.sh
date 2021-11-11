c96870846269822a4a8a86ff0370dc96e092d893
Sebastien La Duca <sladuca777@gmail.com>
2021-11-11 16:51:27 +0000

    add nvm and fuck

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y git-all build-essential zsh neovim mosh
+# packages
+sudo apt install -y git-all build-essential zsh neovim mosh python3-dev python3-pip python3-setuptools
+# rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# nvm/node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+nvm install node
+# fuck - the best command ever
+pip3 install thefuck --user
