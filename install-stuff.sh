32fa3ae594b6c7cd22c677e66e2da949f6d896f3
Sladuca <sladuca777@gmail.com>
2023-01-03 17:53:10 -0500

    fix stuff

--- install-stuff.sh
+++ install-stuff.sh
@@ -5,10 +5,15 @@ SCRIPT_DIR="$(dirname "$0")"
 if [[ $OSTYPE == 'darwin'* ]]
 then
 	xcode-select --install
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+	which -s brew
+	if [[ $? != 0 ]]
+	then
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
 else
 	sudo apt update && sudo apt upgrade
-	sudo apt install git-all build-essential zsh neovim mosh python3-dev python3-pip python3-setuptools
+	sudo apt install git-all build-essential zsh mosh python3-dev python3-pip python3-setuptools
 fi
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
@@ -16,8 +21,6 @@ nvm install node
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup update
-
-curl -L https://foundry.paradigm.xyz | bash
 
 if [[ $OSTYPE == 'darwin'* ]]
 then
