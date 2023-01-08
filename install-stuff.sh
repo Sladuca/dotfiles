ca93044680f3db761579e7fee9d69c9dc49204ae
Sebastien La Duca <sladuca777@gmail.com>
2023-01-08 17:01:20 -0500

    add fzf for linux

--- install-stuff.sh
+++ install-stuff.sh
@@ -13,7 +13,7 @@ then
 	fi
 else
 	sudo apt update && sudo apt upgrade
-	sudo apt install git-all build-essential zsh mosh python3-dev python3-pip python3-setuptools neovim
+	sudo apt install git-all build-essential zsh mosh python3-dev python3-pip python3-setuptools neovim fzf
 
     curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
     sudo dpkg -i ripgrep_13.0.0_amd64.deb
